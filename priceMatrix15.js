6398bc5b55259e42744009f276a4fb80994f89b0
Basti <cimerawow@gmail.com>
2025-07-04 18:20:27 +0200

    Add files via upload

---
+++ priceMatrix15.js
@@ -0,0 +1,23 @@
+const priceMatrixdatei = {
+    "15": [
+        {"minTH":1,"pricePerTH":30.91,"minerCost":32.99},
+        {"minTH":2,"pricePerTH":30.7},
+        {"minTH":4,"pricePerTH":30.48},
+        {"minTH":8,"pricePerTH":30.27},
+        {"minTH":16,"pricePerTH":30.06},
+        {"minTH":32,"pricePerTH":29.85},
+        {"minTH":48,"pricePerTH":29.64},
+        {"minTH":64,"pricePerTH":29.43},
+        {"minTH":96,"pricePerTH":29.23},
+        {"minTH":128,"pricePerTH":29.02},
+        {"minTH":192,"pricePerTH":28.82},
+        {"minTH":256,"pricePerTH":28.62},
+        {"minTH":384,"pricePerTH":28.43},
+        {"minTH":512,"pricePerTH":28.23},
+        {"minTH":768,"pricePerTH":28.04},
+        {"minTH":1024,"pricePerTH":27.84},
+        {"minTH":1536,"pricePerTH":27.65},
+        {"minTH":2560,"pricePerTH":27.46},
+        {"minTH":3584,"pricePerTH":27.28}
+    ]
+};
